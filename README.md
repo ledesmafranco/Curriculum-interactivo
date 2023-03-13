66fb7149a0c7706f7faf4eabbcfb3957fdde9ce6
ledesmafranco <ledesmafranco@gmail.com>
2023-03-13 14:31:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Portafolio Adaptable (Responsive) con HTML, CSS , JS y Bootstrap 5
+
+Pagina Que funciona como CV o Portafolio para el trabajo final de Argentina Programa 4.0 y Ticmas Academy
+
+# Partes de la pagina web:
+
+Barra de navegacion
+
+Seccion Hero
+
+Sobre mi
+
+Autodidacta
+
+Proyectos
+
+Cursos
+
+Contacto
+
+Pie de pagina (footer)
+
+# Tecnologías implementadas
+
+Esta página web fue creada con:
+
+* HTML
+* CSS
+* JavaScript 
+* Bootstrap 5
+
+* Además, se incluyeron **Google Fonts** para las fuente y **Bootstrap icons** para los íconos como flechas y logos de redes sociales populares. 
+
+
+
+# Curricul-Vitae
---
+++ colors.js
@@ -0,0 +1,68 @@
+
+/* color blanco */
+document.getElementById("n0").addEventListener("click", function () {
+    /* cambio de fondo */
+    document.getElementById("navColor").style.backgroundColor = "#f5f6f7";
+    document.getElementById("heroColor").style.backgroundColor = "#f5f6f7";
+    document.getElementById("interColor").style.backgroundColor = "#f5f6f7";
+    document.getElementById("coursesColor").style.backgroundColor = "#f5f6f7";
+
+    /* fuentes */
+    document.getElementById("colorOscuro").style.color = "#2c2c2c";
+    document.getElementById("colorHeader").style.color = "#2c2c2c";
+    document.getElementById("link-nav").style.color = "#2c2c2c";
+});
+/* color verde agua */
+document.getElementById("n1").addEventListener("click", function () {
+    /* cambio de fondo */
+    document.getElementById("navColor").style.backgroundColor = "#0eb376";
+    document.getElementById("heroColor").style.backgroundColor = "#0eb376";
+    document.getElementById("interColor").style.backgroundColor = "#0eb376";
+    document.getElementById("coursesColor").style.backgroundColor = "#0eb376";
+
+    /* fuentes */
+    document.getElementById("colorOscuro").style.color = "#f2f2f2";
+    document.getElementById("colorHeader").style.color = "#f2f2f2";
+    document.getElementById("link-nav").style.color = "#f2f2f2";
+});
+/* color rojo */
+document.getElementById("n2").addEventListener("click", function () {
+    /* cambio de fondo */
+    document.getElementById("navColor").style.backgroundColor = "#b65163";
+    document.getElementById("heroColor").style.backgroundColor = "#b65163";
+    document.getElementById("interColor").style.backgroundColor = "#b65163";
+    document.getElementById("coursesColor").style.backgroundColor = "#b65163";
+
+    /* fuentes */
+    document.getElementById("colorOscuro").style.color = "#f2f2f2";
+    document.getElementById("colorHeader").style.color = "#f2f2f2";
+    document.getElementById("link-nav").style.color = "#f2f2f2";
+
+});
+/* color cian */
+document.getElementById("n3").addEventListener("click", function () {
+    /* cambio de fondo */
+    document.getElementById("navColor").style.backgroundColor = "#63abee";
+    document.getElementById("heroColor").style.backgroundColor = "#63abee";
+    document.getElementById("interColor").style.backgroundColor = "#63abee";
+    document.getElementById("coursesColor").style.backgroundColor = "#63abee";
+
+    /* fuentes */
+    document.getElementById("navColor").style.color = "#f2f2f2";
+    document.getElementById("colorHeader").style.color = "#f2f2f2";
+    document.getElementById("link-nav").style.color = "#f2f2f2";
+});
+
+/* color naranja */
+document.getElementById("n4").addEventListener("click", function () {
+    /* cambio de fondo */
+    document.getElementById("navColor").style.backgroundColor = "#e96d34";
+    document.getElementById("heroColor").style.backgroundColor = "#e96d34";
+    document.getElementById("interColor").style.backgroundColor = "#e96d34";
+    document.getElementById("coursesColor").style.backgroundColor = "#e96d34";
+
+    /* fuentes */
+    document.getElementById("navbar-toggler").style.color = "#2c2c2c";
+    document.getElementById("colorHeader").style.color = "#2c2c2c";
+    document.getElementById("link-nav").style.color = "#2c2c2c";
+});
